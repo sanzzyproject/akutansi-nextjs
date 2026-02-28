4fab5889d49cf235597a6f4639386deb3b7fc8dd
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-01 00:28:07 +0700

    Create tooltip.tsx

---
+++ components/ui/tooltip.tsx
@@ -0,0 +1,7 @@
+"use client"
+import * as React from "react"
+
+export const TooltipProvider = ({ children }: { children: React.ReactNode }) => <>{children}</>
+export const Tooltip = ({ children }: { children: React.ReactNode }) => <>{children}</>
+export const TooltipTrigger = ({ children }: { children: React.ReactNode }) => <>{children}</>
+export const TooltipContent = ({ children }: { children: React.ReactNode }) => <div>{children}</div>
