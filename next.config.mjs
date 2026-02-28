dbeb144b42f09dda7604841297cfd960e44bbfff
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-01 00:00:35 +0700

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // Konfigurasi ini memastikan aplikasi siap untuk Vercel
+};
+
+export default nextConfig;
