da038a01942a288cc54c5e0eec86b8c9e13245d4
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-01 07:49:34 +0700

    Update page.tsx

--- app/laporan/page.tsx
+++ app/laporan/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Skeleton } from '@/components/ui/skeleton';
