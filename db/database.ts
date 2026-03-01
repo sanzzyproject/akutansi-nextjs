d669f28863ed566a97ffc2d7fa4276047eff4d4b
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-01 12:28:49 +0700

    Update database.ts

--- db/database.ts
+++ db/database.ts
@@ -3,11 +3,10 @@ import type { Transaksi, AnalisisRecord } from './types';
 
 class AkuntansiDB extends Dexie {
   transaksi!: Table<Transaksi, number>;
-  analisis!: Table<AnalisisRecord, number>; // TABEL BARU
+  analisis!: Table<AnalisisRecord, number>;
 
   constructor() {
     super('AkuntansiLKS');
-    // Versi dinaikkan ke 2 karena ada tabel baru
     this.version(2).stores({
       transaksi: '++id, tanggal, keterangan',
       analisis: '++id, tanggal, transaksi',
@@ -16,3 +15,8 @@ class AkuntansiDB extends Dexie {
 }
 
 export const db = new AkuntansiDB();
+
+// Dikembalikan agar layanan API tidak error saat proses build Vercel
+export const dataDummy: Omit<Transaksi, 'id'>[] = [
+  { tanggal: '2026-01-01', keterangan: 'Investasi awal pemilik', kas: 50000000, perlengkapan: 0, peralatan: 0, utangUsaha: 0, modal: 50000000 }
+];
