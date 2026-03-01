fd1fa9dc9c20ed0a0f32fcdc9b731e4fd3ae7f37
saannndec5ty <stokakunsanzz250@gmail.com>
2026-03-01 12:15:37 +0700

    Update database.ts

--- db/database.ts
+++ db/database.ts
@@ -1,24 +1,18 @@
 import Dexie, { type Table } from 'dexie';
-import type { Transaksi } from './types';
+import type { Transaksi, AnalisisRecord } from './types';
 
 class AkuntansiDB extends Dexie {
   transaksi!: Table<Transaksi, number>;
+  analisis!: Table<AnalisisRecord, number>; // TABEL BARU
 
   constructor() {
     super('AkuntansiLKS');
-    this.version(1).stores({
+    // Versi dinaikkan ke 2 karena ada tabel baru
+    this.version(2).stores({
       transaksi: '++id, tanggal, keterangan',
+      analisis: '++id, tanggal, transaksi',
     });
   }
 }
 
 export const db = new AkuntansiDB();
-
-export const dataDummy: Omit<Transaksi, 'id'>[] = [
-  { tanggal: '2026-01-01', keterangan: 'Investasi awal pemilik', kas: 50000000, perlengkapan: 0, peralatan: 0, utangUsaha: 0, modal: 50000000 },
-  { tanggal: '2026-01-03', keterangan: 'Pembelian perlengkapan tunai', kas: -2000000, perlengkapan: 2000000, peralatan: 0, utangUsaha: 0, modal: 0 },
-  { tanggal: '2026-01-05', keterangan: 'Pembelian peralatan kredit', kas: 0, perlengkapan: 0, peralatan: 15000000, utangUsaha: 15000000, modal: 0 },
-  { tanggal: '2026-01-10', keterangan: 'Pendapatan jasa tunai', kas: 8000000, perlengkapan: 0, peralatan: 0, utangUsaha: 0, modal: 8000000 },
-  { tanggal: '2026-01-15', keterangan: 'Pembayaran utang usaha', kas: -5000000, perlengkapan: 0, peralatan: 0, utangUsaha: -5000000, modal: 0 },
-  { tanggal: '2026-01-20', keterangan: 'Beban gaji karyawan', kas: -3000000, perlengkapan: 0, peralatan: 0, utangUsaha: 0, modal: -3000000 },
-];
